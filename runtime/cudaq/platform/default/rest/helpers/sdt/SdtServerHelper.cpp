6cdbf449d98f2c18cdeb7d60fb4a83bcfcb74110
hjpark <hjpark@sdt.inc>
2025-06-17 14:52:48 +0900

    Job API 서버로 전송 기능 추가

--- runtime/cudaq/platform/default/rest/helpers/sdt/SdtServerHelper.cpp
+++ runtime/cudaq/platform/default/rest/helpers/sdt/SdtServerHelper.cpp
@@ -35,7 +35,7 @@ std::string searchAPIKey1(std::string &key, std::string &refreshKey,
 class SdtServerHelper : public ServerHelper {
 protected:
   /// @brief The base URL
-  std::string baseUrl = "https://api.sdt.cloud:5000/";  // hjpark: 수정 필요
+  std::string baseUrl = "https://api.anyon.cloud:5000/";  // hjpark: 주소 수정 필요
   /// @brief The machine we are targeting.
   std::string machine = "telegraph-8q"; //"berkeley-25q";//
   /// @brief Time string, when the last tokens were retrieved
@@ -48,6 +48,15 @@ protected:
 
   std::string userSpecifiedCredentials = "";
   std::string credentialsPath = "";
+  std::string configHjpark = "";
+
+  // hjpark
+  // std::string QUREA_BASE_URL_DEFAULT = "https://qurea-api-local.sdt.services";      // dev-server
+  std::string QUREA_BASE_URL_DEFAULT = "http://qurea-api-local.sdt.services:30001"; // local-server
+  std::string QUREA_API_GATEWAY_PATH = "/services/job";
+  std::string QUREA_ENGINE_JOB_PATH = "/api/quantum-jobs";
+  std::string qureaBaseUrl = "";
+  std::string qureaApiKey = "";
 
   /// @brief The ServerHelper requires the API token be updated every so often,
   /// using the provided refresh token. This function will do that.
@@ -57,14 +66,45 @@ protected:
   RestHeaders generateRequestHeader() const;
   RestHeaders generateRequestHeader(std::string) const;
 
+  void postMessage(ServerMessage body);
+
 public:
   /// @brief Return the name of this server helper, must be the
   /// same as the qpu config file.
   const std::string name() const override { return "sdt"; }
   RestHeaders getHeaders() override;
 
+  // hjpark: 예를 들어 .py 에서 아래와 같이 호출되었을 때,
+  // cudaq.set_target("sdt", machine="qubesim", url="localhost", credentials="/sdt/config/.sdt_config")
+  // "sdt" 뒤에 전달되는 key-value 설정값들을 받을 수 있는 메서드다. 
+  // key 값은 정하기 나름이라, 어떻게 쓸지 필요한 대로 수정만 하면 됌.
   void initialize(BackendConfig config) override {
+    // hjpark: Get system environments
+    char* envQureaApiKey = getenv("QUREA_API_KEY");
+    if (envQureaApiKey == NULL) {
+      std::cout << "[SDT] envQureaApiKey is NULL" << std::endl;
+      qureaApiKey = "";
+      // return;
+    } else {
+      qureaApiKey = std::string(envQureaApiKey);
+    }
+    std::cout << "[SDT] QUREA_API_KEY: " << qureaApiKey << std::endl;
+
+    char* envQureaBaseUrl = getenv("QUREA_BASE_URL");
+    if (envQureaBaseUrl == NULL) {
+      std::cout << "[SDT] envQureaBaseUrl is NULL" << std::endl;
+      qureaBaseUrl = QUREA_BASE_URL_DEFAULT;
+    } else {
+      qureaBaseUrl = std::string(envQureaBaseUrl);
+    }
+    std::cout << "[SDT] QUREA_BASE_URL: " << qureaBaseUrl << std::endl;
+
+    // hjpark: Get user configurations
     backendConfig = config;
+    std::cout << "[SDT] backendConfig: \n";
+    for (const auto& [key, value] : backendConfig) {
+        std::cout << "  " << key << ": " << value << std::endl;
+    }
 
     // Set the machine
     auto iter = backendConfig.find("machine");
@@ -79,9 +119,15 @@ public:
         baseUrl += "/";
     }
 
+    // Set credentials
     iter = backendConfig.find("credentials");
     if (iter != backendConfig.end())
       userSpecifiedCredentials = iter->second;
+
+    // Set user config
+    iter = backendConfig.find("configHjpark");
+    if (iter != backendConfig.end())
+      configHjpark = iter->second;
 
     parseConfigForCommonParams(config);
   }
@@ -109,34 +155,61 @@ public:
                           std::string &passPipeline) override;
 };
 
+// hjpark: Executor.cpp에서 호출되며, Executor.cpp에서 API 서버로 호출함. response로 task_id를 받고 일단 끝. 
+//         이후에 아래에 있는 jobIsDone 메서드를 통해 polling으로 종료를 체크함.
 ServerJobPayload
 SdtServerHelper::createJob(std::vector<KernelExecution> &circuitCodes) {
-
   std::vector<ServerMessage> messages;
   for (auto &circuitCode : circuitCodes) {
     // Construct the job itself
     ServerMessage j;
     j["machine"] = machine;
     j["language"] = "QIR 1.0";
-    j["program"] = circuitCode.code;
+    j["program"] = circuitCode.code;    // QIR1.0 을 base64로 인코딩한 값이 저장됨.
     j["priority"] = "normal";
     j["count"] = shots;
     j["options"] = nullptr;
     j["name"] = circuitCode.name;
     messages.push_back(j);
+
+    std::cout << "[SDT] Message: \n" << j << std::endl;
+    /* 예시
+      {
+        "count": 1000,
+        "language": "QIR 1.0",
+        "machine": "telegraph-8q",
+        "name": "kernel",
+        "options": null,
+        "priority": "normal",
+        "program": "QkPA3jUUAAAFAAAAYgwwJEpZvmat+7SvC1GATAEAAAAhDAAAUgIAAAsCIQACAAAAFgAAAAeBI5FByARJBhAyOZIBhAwlBQgZHgSLYoAURQJCkgtCpBAyFDgIGEsKMlKISHDEISNEEoeMEEGSAmTICLEUIENGiCDJATJShBgqKCqQMXywXJEgxcgAAACJIAAAGQAAADIiSAkgYkYAISskmBQhJSSYFBknDIWkkGBSZFwgJGWCIJsjQPQAUBgBMMEggoVKBcg0RgCQMcI4hNBYCYlapjECgJQZxlnECJhhLEfPDOMcinMEgRHmIjoQMEcABnMEoAAAAABRGAAAPwAAABtOIvj/////YSgHd6AHeciHX4CHd0gHd6AHgHiHeqAHeKgHevgFdwiHdiiHeQB2YRd2AQ7YYBfggA3EQB7gAA7gAA7gQAx6oRfYYBfigA12IQ7YQAzkAQ7gAA7gIA7EoBd6oRd6ASDiIR3k4RfgQR7ewRzSgR3KYR6AcCCHcIAHekgHeyiHX4AHeXgHc0gHdigHgJCHcoiHekgHeSgHcoiFehCHdKCHeQDkACDkoRzioR7SQR7KgRxkoRzmoR7YgR7mAZADYAMk/P////8wpIM8yEM52EM5yMM8pIM4sEM5ABuIQQDoYANBCMAZbCCKADiDDYYhAGcA0AEAAEkYAAAGAAAAE4JggiAMEwJiQjBMEApjglAQE4ICAAAAE7JwCAd5GAd0sAM6aIN6cId1cId3uAd3aAN2SAd3qAd8aINzcId62DAH5dAG7aAH5dAG6YAHeoAHejAHctAG6RAHdqAHcWAHejAHctAG6WAHdKAHdkAHbZAOcSAHeKAHcSAHeNAG7jAHcqAHdkAHbTALcSAHeKD0gBAhCWTISAkQQCOEYaOyfM0OARZCmExnoBZixwRMwQAlAAAAAAAAQDABwI5pmoIhSgAAAAAAAIBgAoAhlRlgBAAAAQAAgAAAAAAAwAQMqfzgQoAAAAAAAAQAAAAAACZgSFULGAMEgAAAACAAAAAAADABQ6p2GIMHCAAAAABAAAAAAABgAoZUMKFFQAAAAAAAAgAAAAAAEzCkUoswoIAAAAAAAAQAAAAAACZAYoNAYSsBAIAsEAAAAAsAAAAyHpgUGRFMkIwJJkfGBEOaIwA1QLcIyqAEymEEgMIaEU3TNL1GRNM0XQMAALEYAAC5AAAAMwiAHMThHGYUAT2IQziEw4xCgAd5eAdzmHEM5gAP7RAO9IAOMwxCHsLBHc6hHGYwBT2IQziEgxvMAz3IQz2MAz3MeIx0cAd7CAd5SIdwcAd6cAN2eIdwIIcZzBEO7JAO4TAPbjAP4/AO8FAOMxDEHd4hHNghHcJhHmYwiTu8gzvQQzm0Azy8gzyEAzvM8BR2YAd7aAc3aIdyaAc3gIdwkIdwYAd2KAd2+AV2eId3gIdfCIdxGIdymId5mIEs7vAO7uAO9cAO7DADYsihHOShHMyhHOShHNxhHMohHMSBHcphBtaQQznIQzmYQznIQzm4wziUQziIAzuUwy+8gzz8gjvUAzuwwwzHaYdwWIdycIN0aAd4YId0GId0oIcZzlMP7gAP8lAO5JAO40AP4SAO7FAOMyAoHdzBHsJBHtIhHNyBHtzgHOThHeoBHmYYUTiwQzqcgzvMUCR2YAd7aAc3YId3eAd4mFFM9JAP8FAOMx5qHsphHOghHd7BHX4BHuShHMwhHfBhBlSFgzjMwzuwQz3QQzn8wjzkQzuIwzuww4zFCod5mId3GId0CAd6KAdymIFc4xAO7MAO5VAO8zAjwdJBHuThF9jhHd4BHmZIGTuwgz20gxuEwziMQznMwzy4wTnIwzvUAzzMSLRxCAd2YAdxCIdxWIcZ28YO7GAP7eAG8CAP5TAP5SAP9lAObhAO4zAO5TAP8+AG6eAO5FAO+DAj4uxhHMKBHdjhF+whHeYhHcQhHdghHeghH2YgnTu8Qz24AzmUgznMWLxwcAd3eAd6CAd6SId3cIcZy+cO7zAP4eAO6UAP6aAP5TDDAQNzqAd3GIdfmIdwcId0oId00IdymIGEQTngwziwQz2QQznMQMSgHcqhHeBBHt7BHGYkYzAO4cAO7DAP6UAP5TBDIYN1GAdzSIdfoId8gIdymLGUATyMwzyUwzjQQzq8gzvMw4zFDEghFUJhHuYhHc7BHVKBFAB5IAAAZAAAAHIeSCBDiAwZCXIySCAjgYyRkdFEoBAoZDwxMkKOkCGj2DD3AVIUYdnjQJYjQRMAAERlYnVnIEluZm8gVmVyc2lvbnFpcl9tYWpvcl92ZXJzaW9ucWlyX21pbm9yX3ZlcnNpb25keW5hbWljX3F1Yml0X21hbmFnZW1lbnRkeW5hbWljX3Jlc3VsdF9tYW5hZ2VtZW50cXViaXRfcmVzZXR0aW5nY2xhc3NpY2FsX2ludHNjbGFzc2ljYWxfZmxvYXRzY2xhc3NpY2FsX2ZpeGVkX3BvaW50c3VzZXJfZnVuY3Rpb25zZHluYW1pY19mbG9hdF9hcmdzZXh0ZXJuX2Z1bmN0aW9uc2JhY2t3YXJkc19icmFuY2hpbmcAIwiXMoJwLSMIFzOCcDUjCJczgjAhIwhTMsPgBM8MAyRAMwzRIM0wQMQ0wwAV0wwDZFAzDNAxzTBAyDTDACXTDAOkTDMM0DLNMEDMNMMANZOMBCYoIzY2uzaXtjeyOrYyFzO2sLO5URqqsi4s0zau8z4wAACpGAAAJwAAAAsKciiHd4AHelhwmEM9uMM4sEM50MOC5hzGoQ3oQR7CwR3mIR3oIR3ewR0WNONgDudQD+EgD+RAD+EgD+dQDvSwgIEHeSiHcGAHdniHcQgHeigHclhwnMM4tAE7pIM9lMMCaxzYIRzc4RzcIBzkYRzcIBzogR7CYRzQoRzIYRzCgR3YYcEBD/QgD+FQD/SADguIdRgHc0gHAAAAANEQAAAGAAAAB8w8pIM7nAM7lAM9oIM8lEM4kMMBAAAAYSAAACMAAAATBEEsEAAAAAkAAACERwBKgM4IgCmWoDUCYIolyA11BAKARVg01BEIARZhEQAAAAAjBgUQgmCAWMaIgTGEIBhEk1GMGBhECIIBUxnEiIExhCAYPBQhjBgUQAiCASURIwZGEYJgwFgGMWJgGCEIBk9FCCMGxRGCYEBNhIYDAQAAAAEAAAAHENAAAAAAAHEgAAADAAAAMg4QIoQCsQQAAAAAAAAAAGUMAABJAAAAEgOUOAIAAAADAAAAywAAAC8AAABMAAAAAQAAAFgAAAAAAAAAWAAAAAgAAAAYAQAAAAAAAPoAAAAZAAAAEwEAABEAAAATAAAAAAAAABgBAAAAAAAAAAAAAAgAAAAAAAAAJgAAABgAAAAmAAAAGAAAAP////8AJAAAPgAAABcAAAA+AAAAFwAAAP////8IJAAAVQAAABgAAABVAAAAGAAAAP////8IJAAAbQAAACMAAABtAAAAIwAAAP////8IJAAAkAAAABoAAACQAAAAGgAAAP////8IJAAAqgAAACEAAACqAAAAIQAAAP////8IJAAAJAEAABUAAAAAAAAAEwAAAP////8ACAAAOQEAABUAAAATAAAAEwAAAP////8ACAAAAAAAAF0MAABXAAAAEgOUrgIAAABjc3RyLjcyMzAzMDMwMzAzMDAwY3N0ci43MjMwMzAzMDMwMzEwMF9fbnZxcHBfX21saXJnZW5fX2tlcm5lbF9fcXVhbnR1bV9fcWlzX19oX19ib2R5X19xdWFudHVtX19xaXNfX216X19ib2R5X19xdWFudHVtX19ydF9fcmVzdWx0X3JlY29yZF9vdXRwdXRfX3F1YW50dW1fX3Fpc19fY25vdF9fYm9keV9fcXVhbnR1bV9fcWlzX19yZWFkX3Jlc3VsdF9fYm9keTE2LjAuNiA3Y2JmMWEyNTkxNTIwYzI0OTFhYTM1MzM5ZjIyNzc3NWY0ZDNhZGY2YWFyY2g2NC11bmtub3duLWxpbnV4LWdudUxMVk1EaWFsZWN0TW9kdWxlLkxjc3RyLjcyMzAzMDMwMzAzMDAwLkxjc3RyLjcyMzAzMDMwMzAzMTAwAAAAAAAA"
+      }
+    */
+
+    // hjpark
+    // if (token) {
+      postMessage(j);
+      std::cout << "[SDT] API Request Completed" << std::endl;
+    // }
   }
 
+  /*
   // Get the tokens we need
-  credentialsPath = searchAPIKey1(apiKey, refreshKey, credentials, timeStr,
-                                 userSpecifiedCredentials);
+  credentialsPath = searchAPIKey1(apiKey, refreshKey, credentials, timeStr, userSpecifiedCredentials);
   refreshTokens();
-
+  
   // Get the headers
   RestHeaders headers = generateRequestHeader();
+  */
 
-  cudaq::info(
-      "Created job payload for sdt, language is QIR 1.0, targeting {}",
-      machine);
+  // Remote Backend 로 전송하기 위한 RestHeaders
+  RestHeaders headers{        // RestHeaders => map<string, string>
+    {"Authorization", qureaApiKey},
+    {"Content-Type", "application/json"},
+    // {"Connection", "keep-alive"},
+    // {"Accept", "*/*"}
+  };
+
+  cudaq::info("Created job payload for sdt, language is QIR 1.0, targeting {}", machine);
 
   // return the payload
   return std::make_tuple(baseUrl + "job", headers, messages);
@@ -161,6 +234,7 @@ std::string SdtServerHelper::constructGetJobPath(std::string &jobId) {
   return baseUrl + "job/" + jobId;
 }
 
+// hjpark: 여기서 작업이 끝났는지 polling으로 체크. future.cpp에서 호출 중
 bool SdtServerHelper::jobIsDone(ServerMessage &getJobResponse) {
   auto status = getJobResponse[0]["status"]
                     .get<std::string>(); // All job get and post responses at an
@@ -178,6 +252,7 @@ bool SdtServerHelper::jobIsDone(ServerMessage &getJobResponse) {
     return status == "done";
 }
 
+// hjpark: future.cpp에서 호출하는 jobIsDone이 true가 되면 이 메서드가 실행되서 결과 받아옴
 cudaq::sample_result
 SdtServerHelper::processResults(ServerMessage &postJobResponse,
                                   std::string &jobId) {
@@ -316,6 +391,28 @@ SdtServerHelper::generateRequestHeader(std::string authKey) const {
       {"Connection", "keep-alive"},
       {"Accept", "*/*"}};
   return headers;
+}
+
+// hjpark
+void SdtServerHelper::postMessage(ServerMessage body) { // ServerMessage => nlohmann::json
+  RestClient client;
+  RestHeaders headers{        // RestHeaders => map<string, string>
+    {"Authorization", qureaApiKey},
+    {"Content-Type", "application/json"},
+    {"Connection", "keep-alive"},
+    {"Accept", "*/*"}
+  };
+
+  std::string url = qureaBaseUrl;
+  std::string path = QUREA_API_GATEWAY_PATH + QUREA_ENGINE_JOB_PATH;
+  std::cout << "[SDT] -------------------------- \n" << std::endl;
+  std::cout << "[SDT] QUREA API url: \n" << url << std::endl;
+  std::cout << "[SDT] QUREA API path: \n" << path << std::endl;
+
+  ServerMessage response = client.post(url, path, body, headers);
+  std::cout << "[SDT] Job API POST response: \n" << response << std::endl;
+  // ServerMessage response1 = client.get(url, path, headers);
+  // std::cout << "[SDT] Job API GET response: \n" << response1 << std::endl;
 }
 
 RestHeaders SdtServerHelper::getHeaders() { return generateRequestHeader(); }
